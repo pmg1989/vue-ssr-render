1f0f188910083f091bb9b19fed0d001bf341a2cd
felixpmg <felix.pan@newband.com>
2017-11-01 18:03:46 +0800

    fix node env

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <header class="header">
+      hello world!!!
+    </header>
+    <transition name="fade" mode="out-in">
+      <router-view class="view"></router-view>
+    </transition>
+  </div>
+</template>
+
+<style scoped>
+  .header {
+    text-align: center;
+  }
+</style>
