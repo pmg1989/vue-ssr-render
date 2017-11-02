749f7c311c5c2648502505322a063937ceba68cc
felixpmg <felix.pan@newband.com>
2017-11-02 11:48:18 +0800

    fix node env

--- src/entry-client.js
+++ src/entry-client.js
@@ -2,7 +2,25 @@ import Vue from 'vue'
 import 'es6-promise/auto'
 import { createApp } from './app'
 
-// a global mixin that calls `asyncData` when a route component's params change
+// 客户端数据预取(Client Data Fetching)
+
+// 匹配要渲染的视图后，再获取数据(如需实现这种客户端数据预取策略，解注此段代码，并注释 router.beforeResolve相关代码即可)
+// Vue.mixin({
+//   beforeMount () {
+//     const { asyncData } = this.$options
+//     if (asyncData) {
+//       // 将获取数据操作分配给 promise
+//       // 以便在组件中，我们可以在数据准备就绪后
+//       // 通过运行 `this.dataPromise.then(...)` 来执行其他任务
+//       this.dataPromise = asyncData({
+//         store: this.$store,
+//         route: this.$route
+//       })
+//     }
+//   }
+// })
+
+// 当路由组件重用（同一路由，但是 params 或 query 已更改，例如，从 user/1 到 user/2）时，也应该调用 asyncData 函数
 Vue.mixin({
   beforeRouteUpdate (to, from, next) {
     const { asyncData } = this.$options
@@ -26,6 +44,7 @@ if (window.__INITIAL_STATE__) {
 
 // 这里假定 App.vue 模板中根元素具有 `id="app"`
 router.onReady(() => {
+  // 在路由导航之前解析数据
   // 添加路由钩子函数，用于处理 asyncData.
   // 在初始路由 resolve 后执行，
   // 以便我们不会二次预取(double-fetch)已有的数据。
