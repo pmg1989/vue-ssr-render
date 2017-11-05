4102d833c3fa5bfe38a16dfdef240d053dd689d0
pmg1989 <972401854@qq.com>
2017-11-05 10:12:08 +0800

    fix node env

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -19,15 +19,15 @@ const config = merge(base, {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"client"'
     }),
-    // extract vendor chunks for better caching
+    // 将依赖模块提取到 vendor chunk 以获得更好的缓存，是很常见的做法。
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       minChunks: function (module) {
-        // a module is extracted into the vendor chunk if...
+        // 一个模块被提取到 vendor chunk 时……
         return (
-          // it's inside node_modules
+          // 如果它在 node_modules 中
           /node_modules/.test(module.context) &&
-          // and not a CSS file (due to extract-text-webpack-plugin limitation)
+          // 如果 request 是一个 CSS 文件，则无需外置化提取
           !/\.css$/.test(module.request)
         )
       }
